3de69720c7f132ee29d5d473231f89da765a6d32
DavidWells <hello@davidwells.io>
2018-07-31 15:30:39 -0700

    add functions

---
+++ functions/form-handler.js
@@ -0,0 +1,13 @@
+
+exports.handler = (event, context, callback) => {
+  console.log('event', event)
+  console.log('context', context)
+
+  return callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({
+      data: 'foo'
+    })
+  })
+  
+}
