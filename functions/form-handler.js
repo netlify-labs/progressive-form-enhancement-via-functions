6ff422fdb3c121c31f7674bb897d48ee0dcfed5c
DavidWells <hello@davidwells.io>
2018-10-01 15:48:21 -0700

    add code

--- functions/form-handler.js
+++ functions/form-handler.js
@@ -1,13 +1,44 @@
+import { parse } from 'querystring'
 
 exports.handler = (event, context, callback) => {
-  console.log('event', event)
-  console.log('context', context)
+  let body = {}
+  console.log(event)
+  try {
+    body = JSON.parse(event.body)
+  } catch (e) {
+    body = parse(event.body)
+  }
 
+  // Bail if email is missing
+  if (!body.email) {
+    return callback(null, {
+      statusCode: 400,
+      body: JSON.stringify({
+        error: 'missing email'
+      })
+    })
+  }
+
+  // Do my email subscription logic
+  
+
+  if (event.headers['content-type'] === 'application/x-www-form-urlencoded') {
+    // Do redirect for non JS enabled browsers
+    return callback(null, {
+      statusCode: 302,
+      headers: {
+        Location: '/thanks.html',
+        'Cache-Control': 'no-cache',
+      },
+      body: JSON.stringify({})
+    })
+  }
+
+  // Return data to AJAX request
   return callback(null, {
     statusCode: 200,
     body: JSON.stringify({
-      data: 'foo'
+      emailAdded: true
     })
   })
-  
 }
